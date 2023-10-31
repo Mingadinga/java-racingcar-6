679ce838575a63bce87855755539bcd56ceef5d5
mingadinga <minpearl0826@gmail.com>
2023-11-01 01:17:33 +0900

    style: [참가자 등록] ParticipantNameInputComponent 필요하지 않은 메시지 제거, 메시지 구현하지 않은 이유 주석 추가

--- src/main/java/racingcar/participant/view/ParticipantNameInputComponent.java
+++ src/main/java/racingcar/participant/view/ParticipantNameInputComponent.java
@@ -4,10 +4,12 @@ import racingcar.view.ConsoleInputComponent;
 
 public class ParticipantNameInputComponent extends ConsoleInputComponent {
 
-    private final static String MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-
     @Override
     protected void validate(String input) {
+        /**
+         * token으로 분리해 name validate 필요.
+         * Controller에서 주입 받은 Validator가 처리함.
+         */
 
     }
 }
